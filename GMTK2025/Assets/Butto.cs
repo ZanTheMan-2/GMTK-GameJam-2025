9a66e44ab3b87f535c5dc122d4faf905531684f4
ZanTheMan <vanwyk.zander05@gmail.com>
2025-08-02 23:33:35 +0200

    main menu

---
+++ GMTK2025/Assets/Butto.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Butto : MonoBehaviour
+{
+
+ public void playButton()
+    {
+        SceneManager.LoadScene("Week 1");
+    }
+
+    
+}
