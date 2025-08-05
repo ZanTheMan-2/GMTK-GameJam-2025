595bcf6df8b1c2928eead5ca325fc333b2f54824
ZanTheMan <vanwyk.zander05@gmail.com>
2025-08-05 11:53:11 +0200

    This aint much

--- GMTK2025/Assets/Butto.cs
+++ GMTK2025/Assets/Butto.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 using TMPro;
 public class Butto : MonoBehaviour
 {
+    public TMP_Text triggerText;
 
  public void playButton()
     {
         SceneManager.LoadScene("Week 1");
     }
 
-    
+    public void triggerButton()
+    {
+        triggerText.enabled = true;
+    }
 }
